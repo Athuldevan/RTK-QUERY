07ff19578da03fb1495710b90adf7924b51aa63e
Athul <athuldevan90@gmail.com>
2025-11-11 11:09:06 +0530

    Added SignUp Feature and component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import Login from "./components/login";
+import Singup from "./components/Singup";
 import TodoList from "./components/TodoList";
 import { useGetTodoQuery } from "./services/todoApiSlice";
 
 function App() {
+  const [signUp, setSignUp] = useState(false);
   const { data, isLoading, isError } = useGetTodoQuery({});
 
   if (isLoading) return <div>Loading</div>;
   if (isError) return <div>Error in fetching</div>;
   return (
     <>
-      <Login/>
+      <Login />
+      <button
+        onClick={() => setSignUp(!signUp)}
+        className=" border-8 bg-pink-500 ml-220"
+      >
+        Show Signup
+      </button>
+      {signUp && <Singup />}
       <TodoList todos={data.todos} />
     </>
   );
---
+++ src/components/Singup.tsx
@@ -0,0 +1,11 @@
+
+
+const Singup = () => {
+    return (
+        <div className="bg-amber-500 text-8xl text-teal-50">
+            <h3>Signup</h3>
+        </div>
+    );
+}
+
+export default Singup;
